4baddb537e5ff3a270551768a7078ee87efb8623
Razvan Balint <razvannbalint@gmail.com>
2016-04-08 15:45:35 +0300

    Initial Commit

---
+++ playfair/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  playfair
+//
+//  Created by Razvan Balint on 08/04/16.
+//  Copyright © 2016 Razvan Balint. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
